3e24ded55e3afcba7c748d21272c79c5b9e8bd4e
ShradhaV08 <sv4119@srmist.edu.in>
2026-03-05 09:11:23 +0530

    BookMyStayApp

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+class UC1_ApplicationEntryAndWelcomeMessage{
+
+    public static void main(String[] args) {
+
+        System.out.println("============================================================");
+        System.out.println("        Welcome to Book My Stay App                        ");
+        System.out.println("        Hotel Booking Management System  v1.0              ");
+        System.out.println("============================================================");
+        System.out.println("  Initializing Hotel Booking Management System...          ");
+        System.out.println("  Application started successfully.                        ");
+        System.out.println("============================================================");
+
+    }
+}

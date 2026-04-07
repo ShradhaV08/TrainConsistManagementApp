4caaf5f87f6efd242870469aa2ab1b7865fe5d33
ShradhaV08 <sv4119@srmist.edu.in>
2026-04-07 09:09:32 +0530

    TrainConsistManagementApp

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+
+import java.util.*;
+
+class Bogie {
+    private String name;
+    private int capacity;
+
+    public Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("UC7 - Sort Bogies by Capacity (Comparator)");
+        System.out.println("======================================\n");
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("General", 90));
+
+        System.out.println("Before Sorting:");
+        for (Bogie b : bogies) {
+            System.out.println(b.getName() + " -> " + b.getCapacity());
+        }
+
+        bogies.sort(Comparator.comparingInt(Bogie::getCapacity));
+
+        System.out.println("\nAfter Sorting by Capacity:");
+        for (Bogie b : bogies) {
+            System.out.println(b.getName() + " -> " + b.getCapacity());
+        }
+
+        System.out.println("\nUC7 sorting completed...");
+    }
+}
